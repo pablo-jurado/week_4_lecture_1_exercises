fbccdf287fd03428cbdf1cb04b00664ad364ca03
Pablo Jurado <pjurado85@gmail.com>
2017-03-08 07:19:02 -0600

    update file main 3

---
+++ main_3.js
@@ -0,0 +1,15 @@
+// a: String
+var result = "The cat jumped over the moon";
+
+// b: Number
+var result = 55 - 27;
+
+// c: Boolean
+var result = 75 > 55;
+
+// d: Undefined
+var data;
+var result = data;
+
+// e: Number
+var result = -6544.432;
